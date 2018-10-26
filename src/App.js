e64ca99d1058df41e5d7ef7e9b2b2e9f5a495e42
Nicolas Terol <nterol@student.42.fr>
2018-10-26 13:18:32 +0200

    works fine on now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import Main from "./pages/Main";
 
-const App = () => <Main />;
+class App extends Component {
+  render() {
+    return <Main />;
+  }
+}
 
 export default App;
